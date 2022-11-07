79a5b314b88794ec22a94216c810039b1f56fcb1
HBA114 <hbasriayhaner114@gmail.com>
2022-11-07 22:15:02 +0300

    Technology create delete and get finished

--- src/main/java/kodlama/io/kodlama_io_devs/entities/ProgrammingLanguage.java
+++ src/main/java/kodlama/io/kodlama_io_devs/entities/ProgrammingLanguage.java
@@ -2,13 +2,17 @@ package kodlama.io.kodlama_io_devs.entities;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,6 +34,7 @@ public class ProgrammingLanguage {
     @Column(name = "name", unique = true)
     private String name;
 
-    @OneToMany(mappedBy = "programmingLanguage")
+    @JsonManagedReference
+    @OneToMany(mappedBy = "programmingLanguage", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Technology> technologies;
 }
